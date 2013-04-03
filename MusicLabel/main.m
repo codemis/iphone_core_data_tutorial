04bcf647a963914885b547b6fba1e2b66a0f7bf0
Codemis <johnathan@missionaldigerati.com>
2013-04-02 19:03:35 -0700

    Initial Commit

---
+++ MusicLabel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MusicLabel
+//
+//  Created by Johnathan Pulos on 4/2/13.
+//  Copyright (c) 2013 Johnathan Pulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MLBLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MLBLAppDelegate class]));
+    }
+}
